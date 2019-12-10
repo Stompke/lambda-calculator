10fab877d4124f86d849f77a8c20bc68236468eb
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2019-12-10 14:26:01 -0800

    MVP project completed.

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OperatorButton = (props) => {
   return (
-    <button>
+    <button value={props.data.value}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.data.char}
     </button>
